5fe6e58a8a72e39f86b097465d15387feeda9ac3
SergioRodriguezRivera <sergioer@bu.edu>
2024-05-05 18:02:21 -0400

    contacts

---
+++ app/src/main/java/com/example/androidp2p/activities/UsersActivity.java
@@ -0,0 +1,95 @@
+package com.example.androidp2p.activities;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.androidp2p.adapters.UsersAdapter;
+import com.example.androidp2p.databinding.ActivityUsersBinding;
+import com.example.androidp2p.models.User;
+import com.example.androidp2p.utilities.Constants;
+import com.example.androidp2p.utilities.PreferenceManager;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsersActivity extends AppCompatActivity {
+
+    private ActivityUsersBinding binding;
+    private PreferenceManager preferenceManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityUsersBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        setListeners();
+        getUsers();
+    }
+
+    private void setListeners(){
+        binding.imageBack.setOnClickListener(v -> {
+            OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled */) {
+                @Override
+                public void handleOnBackPressed() {
+                    // Handle the back button event
+                    finish();
+                }
+            };
+            getOnBackPressedDispatcher().addCallback(this, callback);
+            callback.handleOnBackPressed();
+        });
+    }
+    private void getUsers(){
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .get()
+                .addOnCompleteListener(task -> {
+                    loading(false);
+                    String currentUserId = preferenceManager.getString(Constants.KEY_USER_ID);
+                    if (task.isSuccessful() && task.getResult() != null){
+                        List<User> users = new ArrayList<>();
+                        for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()){
+                            if (currentUserId.equals(queryDocumentSnapshot.getId())) {
+                                continue;
+                            }
+                            User user = new User();
+                            user.name = queryDocumentSnapshot.getString(Constants.KEY_NAME);
+                            user.email = queryDocumentSnapshot.getString(Constants.KEY_EMAIL);
+                            user.image = queryDocumentSnapshot.getString(Constants.KEY_IMAGE);
+                            user.token = queryDocumentSnapshot.getString(Constants.KEY_FCM_TOKEN);
+                            users.add(user);
+                        }
+                        if ( users.size() >0  ){
+                            UsersAdapter usersAdapter = new UsersAdapter(users);
+                            binding.usersRecyclerView.setAdapter(usersAdapter);
+                            binding.usersRecyclerView.setVisibility(View.VISIBLE);
+                        } else {
+                            showErrorMessage();
+                        }
+                    } else {
+                        showErrorMessage();
+                    }
+                });
+    }
+
+    private void showErrorMessage(){
+        binding.textErrorMessage.setText(String.format("%s", "No user available"));
+        binding.textErrorMessage.setVisibility(View.VISIBLE);
+    }
+
+    private void loading(boolean isLoading){
+        if (isLoading){
+            binding.progressBar.setVisibility(View.VISIBLE);
+        } else {
+            binding.progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+}
